18c38eac373dd44bb8db87b8b3690903765025dc
Sabine Maennel <sabine.maennel@gmail.com>
2020-02-24 17:49:47 +0100

    add js to show active chapter in subchapters and to toggle field definitions

--- source/_static/js/custom.js
+++ source/_static/js/custom.js
@@ -32,3 +32,36 @@ $(document).ready(function(){
      $('.ogdch-jumpbox').replaceWith(jumpBox);
   }
 });
+
+// This takes from the custom-breadcrumbs directive which chapter is currently active
+// For this to work: chapters that are in the sidebar must have unique names
+
+$(document).ready(function(){
+  var ogdchActiveBreadcrumb;
+  var ogdchActiveText;
+  ogdchActiveBreadcrumb = $('.custom-breadcrumbs ul li:nth-child(3) a.internal').attr('href');
+  ogdchActiveText = $('.custom-breadcrumbs ul li:nth-child(4) p').text();
+
+  $('.wy-menu-vertical ul li a.internal').each(function () {
+    if ($(this).attr('href') === ogdchActiveBreadcrumb) {
+      $(this).addClass("current custom-breadcrumb-upper");
+      $(this).parent().addClass("current");
+      $(this).parent().append("<ul class='custom-breadcrumb-sub'><li class='custom-breadcrumb-sub'>" + ogdchActiveText+ "</li></ul>");
+    }
+  });
+});
+
+// Attribute definitions toggle
+
+$(document).ready(function(){
+  var attributeHeader = $('.attribute');
+  attributeHeader.each(function() {
+      $(this).children('dl').hide();
+      $(this).children('div').hide();
+      $(this).click(showDefinition);
+      function showDefinition(event) {
+          $(this).children('dl').toggle();
+          $(this).children('div').toggle();
+      }
+  });
+});
