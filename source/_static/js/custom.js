b3c801faf825b8e94d9843dac1624cf963e35973
Maennel <sabine.maennel@liip.ch>
2020-10-19 14:20:14 +0200

    remove jumbbox from glossar/terms since it does not work for the french transalation

--- source/_static/js/custom.js
+++ source/_static/js/custom.js
@@ -3,36 +3,6 @@
 // ---------------------------------------------------------------
 
 // ---------------------------------------------------------------
-// Stichwortverzeichnis
-// ---------------------------------------------------------------
-// generates a jump box A | D | M | for all letters that have
-// terms depends on the headers in the begriffe page: Begriffe
-// and subheader "Begriffe erklärt" if these headers change
-// the ids below must be changed accordingly
-// ---------------------------------------------------------------
-$(document).ready(function(){
-  var ogdchActivePage;
-  ogdchActivePage = $('.custom-breadcrumbs ul li:nth-child(2) p').text();
-  if (ogdchActivePage === 'Begriffe') {
-     var jumpBox = "" +
-         "<div class=\"ogdch-jumpbox\">\n";
-     var startTermCollection = true;
-
-     $('#was-ist-was-bei-open-data .section').each(function() {
-       var letter = $(this).attr('id');
-       if (!startTermCollection) {
-           jumpBox += " | ";
-       } else {
-           startTermCollection = false;
-       }
-       jumpBox += "<a href=\"#" + letter + "\"><strong>" + letter.toUpperCase() + "</strong></a>\n";
-     });
-     jumpBox += "</div>";
-     $('.ogdch-jumpbox').replaceWith(jumpBox);
-  }
-});
-
-// ---------------------------------------------------------------
 // Show active chapter in sidebar even on subpages
 // ---------------------------------------------------------------
 // This takes from the custom-breadcrumbs directive which chapter is currently active
