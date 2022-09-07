db5f3198483eb93f7a8a2be0dd21f2656064bac1
Rae Knowler <rae.knowler@liip.ch>
2022-09-07 15:25:05 +0200

    feat: Exit script with error code if rsync doesn't work

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -5,6 +5,6 @@ eval $(ssh-agent -s)
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
 
 # Rsync the documents to the server
-rsync -a --delete -e "ssh -o StrictHostKeyChecking=no -p ${SSH_PORT}" -v ./build/de/html/ ${SSH_USER}@${SSH_HOST}:${DEST_PATH}/de/
-rsync -a --delete -e "ssh -o StrictHostKeyChecking=no -p ${SSH_PORT}" -v ./build/fr/html/ ${SSH_USER}@${SSH_HOST}:${DEST_PATH}/fr/
+rsync -a --delete -e "ssh -o StrictHostKeyChecking=no -p ${SSH_PORT}" -v ./build/de/html/ ${SSH_USER}@${SSH_HOST}:${DEST_PATH}/de/ || exit 1
+rsync -a --delete -e "ssh -o StrictHostKeyChecking=no -p ${SSH_PORT}" -v ./build/fr/html/ ${SSH_USER}@${SSH_HOST}:${DEST_PATH}/fr/ || exit 1
 echo "-> deploy finished, site available at https://${HOSTNAME}/"
