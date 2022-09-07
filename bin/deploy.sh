afb79d5e90e13795ae33fa297a82e95fd4a06380
Rae Knowler <rae.knowler@liip.ch>
2022-09-07 14:16:31 +0200

    feat: Remove .htaccess configuration per environment
    The Apache config and settings are kept in the private repo for the site hosting, so we can't change them from here.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,7 +6,6 @@ make gettext
 make -e SPHINXOPTS="-D language='de'" BUILDDIR="build/de" html
 make -e SPHINXOPTS="-D language='fr'" BUILDDIR="build/fr" html
 cd ..
-rsync -a --delete -e "ssh -o StrictHostKeyChecking=no -p ${SSH_PORT}" -v docs/build/de/html/                      ${SSH_USER}@${SSH_HOST}:${DEST_PATH}/de/
-rsync -a --delete -e "ssh -o StrictHostKeyChecking=no -p ${SSH_PORT}" -v docs/build/fr/html/                      ${SSH_USER}@${SSH_HOST}:${DEST_PATH}/fr/
-rsync -a          -e "ssh -o StrictHostKeyChecking=no -p ${SSH_PORT}" -v apache/.htaccess${HTACCESS_VARIANT} ${SSH_USER}@${SSH_HOST}:${DEST_PATH}/.htaccess
+rsync -a --delete -e "ssh -o StrictHostKeyChecking=no -p ${SSH_PORT}" -v ./build/de/html/ ${SSH_USER}@${SSH_HOST}:${DEST_PATH}/de/
+rsync -a --delete -e "ssh -o StrictHostKeyChecking=no -p ${SSH_PORT}" -v ./build/fr/html/ ${SSH_USER}@${SSH_HOST}:${DEST_PATH}/fr/
 echo "-> deploy finished, site available at https://${HOSTNAME}/"
