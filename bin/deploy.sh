24c65c4cb346ee57339dfc751924d13e035539a0
Rae Knowler <rae.knowler@liip.ch>
2022-09-07 12:40:15 +0200

    feat: Add workflow and scripts to deploy via GitHub Actions

---
+++ bin/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export ENABLE_HYPOTHESIS=${ENABLE_HYPOTHESIS}
+export ROBOTS_DIRECTIVE=${ROBOTS_DIRECTIVE}
+cd docs
+make clean
+make gettext
+make -e SPHINXOPTS="-D language='de'" BUILDDIR="build/de" html
+make -e SPHINXOPTS="-D language='fr'" BUILDDIR="build/fr" html
+cd ..
+rsync -a --delete -e "ssh -o StrictHostKeyChecking=no -p ${SSH_PORT}" -v docs/build/de/html/                      ${SSH_USER}@${SSH_HOST}:${DEST_PATH}/de/
+rsync -a --delete -e "ssh -o StrictHostKeyChecking=no -p ${SSH_PORT}" -v docs/build/fr/html/                      ${SSH_USER}@${SSH_HOST}:${DEST_PATH}/fr/
+rsync -a          -e "ssh -o StrictHostKeyChecking=no -p ${SSH_PORT}" -v apache/.htaccess${HTACCESS_VARIANT} ${SSH_USER}@${SSH_HOST}:${DEST_PATH}/.htaccess
+echo "-> deploy finished, site available at https://${HOSTNAME}/"
---
+++ bin/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install the ssh private key that will let us access the server
+'which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )'
+eval $(ssh-agent -s)
+echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
+# Install rsync to deploy with
+apt-get update -y && apt-get install rsync -y
+# Change git submodule remote to use https instead of ssh
+sed  -i  "s/url = .*ogdch-handbook.git/url = https:\/\/github.com\/opendata-swiss\/ogdch-handbook.git/" .gitmodules
+git submodule sync
+git submodule update --init
+# Setup Python (printing out version for debugging)
+python -V
+pip install virtualenv
+virtualenv venv
+source venv/bin/activate
+pip install -r docs/requirements.txt
