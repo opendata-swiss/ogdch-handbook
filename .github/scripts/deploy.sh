e025145a16fa33a579939f8efbf2d622bebb5ad7
Rae Knowler <rae.knowler@liip.ch>
2022-09-07 15:29:53 +0200

    feat: Report successful deployment to relevant env

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -7,4 +7,4 @@ echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
 # Rsync the documents to the server
 rsync -a --delete -e "ssh -o StrictHostKeyChecking=no -p ${SSH_PORT}" -v ./build/de/html/ ${SSH_USER}@${SSH_HOST}:${DEST_PATH}/de/ || exit 1
 rsync -a --delete -e "ssh -o StrictHostKeyChecking=no -p ${SSH_PORT}" -v ./build/fr/html/ ${SSH_USER}@${SSH_HOST}:${DEST_PATH}/fr/ || exit 1
-echo "-> deploy finished, site available at https://${HOSTNAME}/"
+echo "-> deployment to ${ENV} environment was successful!"
